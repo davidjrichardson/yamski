08d0ff5d8e208e44e0ef01a9b90277a7d4dce75d
David Richardson <david@tankski.co.uk>
2018-10-15 15:48:44 +0100

    Hit a borrow checker problem, pls help Faux

--- yamski-backend/src/routing.rs
+++ yamski-backend/src/routing.rs
@@ -2,6 +2,8 @@ extern crate rocket;
 
 use std::net::SocketAddr;
 
+use std::sync::Arc;
+
 use rocket::State;
 use rocket_contrib::Json;
 
@@ -16,9 +18,19 @@ fn index(state: State<MusicState>) -> String {
 #[post("/alias", data = "<alias>", format = "application/json")]
 fn update_alias(alias: Json<AliasForm>, state: State<MusicState>, remote: SocketAddr) -> String {
     // TODO: Update the aliase (or insert one if it doesn't exist)
-    format!(
-        "New alias: {} for addr: {:?}\n",
-        alias.into_inner().alias,
-        remote.ip()
-    )
+    let user = state
+        .user_list
+        .read()
+        .unwrap()
+        .into_iter()
+        .find(|item: &Arc<User>| {
+            item.address == remote.ip()
+        });
+
+    match user {
+        Some(u)  => {},
+        None        => {},
+    }
+
+    format!("Changing alias for {:?} to {}\n", remote.ip(), alias.alias)
 }
